c7d71382bcedc50fcbf8a78a4964b029c3fce8ec
jamesdanielwhitford <70632508+jamesdanielwhitford@users.noreply.github.com>
2022-10-28 12:49:44 +0200

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + "/dist/index.html"));
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.HOST_URL || 3000;
 app.listen(port, () => { 
     console.log("listening on port " + port); 
 });
